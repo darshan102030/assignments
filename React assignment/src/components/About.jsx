65dd51ab6cc4cea77dda93c685c13e72f22f1329
darshan102030 <gosavidarshan024@gmail.com>
2025-01-03 01:58:08 +0530

    Create About.jsx

---
+++ React assignment/src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./../styles/About.css";
+const About = () => {
+  return (
+    <section id="about" className="about">
+      <h2>About Us</h2>
+      <p>Trying to spread joy here...</p>
+    </section>
+  );
+};
+export default About;
